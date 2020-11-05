bb4fd8dc0afaefb6778f73ec05d1d19a6016a011
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-11-04 17:44:39 -0700

    added a login, logout, and inactivity javascript file that all will need to be edited at a future date to accomadate the new html and handlebars files to be added to the project. An email file has been added as well and includes pseudocode to implement said feature.

---
+++ public/javascript/login.js
@@ -0,0 +1,26 @@
+//The login and logout functions will need to be altered in order to fit the new html/handlebars
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
---
+++ public/javascript/logout.js
@@ -0,0 +1,15 @@
+//The login and logout functions will need to be altered in order to fit the new html/handlebars
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
