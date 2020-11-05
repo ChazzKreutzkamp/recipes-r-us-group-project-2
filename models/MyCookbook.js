3de9a726e35c77e341d46285a14e30efd214110a
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-11-05 11:30:19 -0700

    add initial version of index, user, MyCookbook, recipes, ingredients, directions, and MyCookbook_Recipes.

---
+++ models/MyCookbook.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class MyCookbook extends Model { }
+
+MyCookbook.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        liked: {
+            type: DataTypes.TINYINT,
+            allowNull: false,
+            defaultValue: 0,
+            validate: {
+                min: -1,
+                max: 1
+            }
+        },
+        recipe_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'recipes',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+
+module.exports = MyCookbook;
