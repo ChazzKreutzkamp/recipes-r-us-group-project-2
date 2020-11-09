108128b9e6289e950d040bf616b8c8c5c143d08e
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-11-09 14:03:14 -0700

    started making the home routes, and added two routes to the landing page and recipe page.

---
+++ controllers/home-routes.js
@@ -0,0 +1,65 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Ingredients, MyCookbook, MyCookbook_Recipes, Recipes, Directions } = require('../models');
+
+router.get('/', (req, res) => {
+    console.log(req.session);
+    Recipes.findAll({
+        where: {
+            featured: 1
+        }
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup');
+});
+
+router.get('/herestherecipe/:id', (req, res) => {
+    Recipes.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Ingredients
+            },
+            {
+                model: Directions
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No recipe found with this id' });
+                return;
+            }
+
+            const recipe = dbPostData.get({ plain: true });
+
+            res.render('recipe', {
+                recipe,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
