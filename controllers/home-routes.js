de72b401cea91e0029aae4afa7275ed3c8e7a3bd
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-11-09 15:50:06 -0700

    dashboard, recipe, edit-recipe, sign up, and landing page routes have been added.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Ingredients, MyCookbook, MyCookbook_Recipes, Recipes, Directions } = require('../models');
+const { use } = require('./api');
 
 router.get('/', (req, res) => {
     console.log(req.session);
@@ -13,6 +14,39 @@ router.get('/', (req, res) => {
             const posts = dbPostData.map(post => post.get({ plain: true }));
             res.render('homepage', {
                 posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/dashboard', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.session.id
+        },
+        include: [
+            {
+                model: MyCookbook,
+                include: {
+                    model: Recipes
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No User found with this id' });
+                return;
+            }
+
+            const user = dbPostData.get({ plain: true });
+
+            res.render('dashboard', {
+                user,
                 loggedIn: req.session.loggedIn
             });
         })
@@ -54,6 +88,38 @@ router.get('/herestherecipe/:id', (req, res) => {
             const recipe = dbPostData.get({ plain: true });
 
             res.render('recipe', {
+                recipe
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/edit-recipe/:id', (req, res) => {
+    Recipes.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Ingredients
+            },
+            {
+                model: Directions
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No recipe found with this id' });
+                return;
+            }
+
+            const recipe = dbPostData.get({ plain: true });
+
+            res.render('editRecipe', {
                 recipe,
                 loggedIn: req.session.loggedIn
             });
