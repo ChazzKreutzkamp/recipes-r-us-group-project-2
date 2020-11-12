478344a2709679af82d1dbcdf8b2589ad5d7f2e9
ChazzKreutzkamp <chazz.kreutzkamp@gmail.com>
2020-11-12 15:19:34 -0700

    the featured card and the featured button/page all work, and logic has been added to the What you wanna eat brand button.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,12 +9,14 @@ router.get('/', (req, res) => {
     Recipes.findAll({
         where: {
             featured: 1
-        }
+        },
+        order: sequelize.literal('rand()'),
+        limit: 1
     })
         .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
+            const recipe = dbPostData.map(post => post.get({ plain: true }));
             res.render('landingpage', {
-                posts,
+                recipe,
                 loggedIn: req.session.loggedIn
             });
         })
@@ -36,6 +38,30 @@ router.get('/homepage', (req, res) => {
             res.render('homepage', {
                 recipe,
                 loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+router.get('/featured', (req, res) => {
+    console.log(req.session);
+    Recipes.findAll({
+        where: {
+            featured: 1
+        },
+        order: sequelize.literal('rand()'),
+        limit: 1
+    })
+        .then(dbPostData => {
+            const recipe = dbPostData.map(post => post.get({ plain: true }));
+            res.render('featured-recipe', {
+                recipe,
+                loggedIn: req.session.loggedIn,
+                user_email: req.session.user_email,
+                isAdmin: req.session.isAdmin
             });
         })
         .catch(err => {
@@ -90,125 +116,125 @@ router.get('/account_info', (req, res) => {
 });
 
 router.get('/search-results/:searchTerm', async (req, res) => {
-        // input
-        var searchTerm = req.params.searchTerm;
-        var searchString = "%" + searchTerm + "%";
-        var searchVal = null;
-        var featured = null;      
+    // input
+    var searchTerm = req.params.searchTerm;
+    var searchString = "%" + searchTerm + "%";
+    var searchVal = null;
+    var featured = null;
 
-        // regex test to check if it is a number
-        var reg = new RegExp('^\\d+$');
-        let isnum = reg.test(req.params.searchTerm);
+    // regex test to check if it is a number
+    var reg = new RegExp('^\\d+$');
+    let isnum = reg.test(req.params.searchTerm);
 
-        // if the user searches for "featured" it will find all featured items
-        if (searchTerm === "featured" || req.body.featured === 1) {
-            featured = 1
-        } else if (searchTerm === "not featured" || req.body.featured === 0) {
-            featured = 0
-        }
+    // if the user searches for "featured" it will find all featured items
+    if (searchTerm === "featured" || req.body.featured === 1) {
+        featured = 1
+    } else if (searchTerm === "not featured" || req.body.featured === 0) {
+        featured = 0
+    }
 
-        if (isnum != "") {
-            searchVal = parseInt(searchTerm)
-        }
+    if (isnum != "") {
+        searchVal = parseInt(searchTerm)
+    }
 
-        Recipes.findAll({
-            where: {
-                [Op.or]: [
-                    { title: { [Op.like]: searchString } },
-                    { featured: featured },
-                    { yield: { [Op.like]: searchString } },
-                    { cook_time: searchVal },
-                    { cuisine: { [Op.like]: searchString } },
-                    { description: { [Op.like]: searchString } },
-                    { direction_list: { [Op.like]: searchString } },
-                    { ingredient_list: { [Op.like]: searchString } }
-                ]
-            },
-            include: [
-                {
-                    model: User,
-                    attributes: ["id", "username"]
-                },
-                { model: Ingredients },
-                { model: Directions }
+    Recipes.findAll({
+        where: {
+            [Op.or]: [
+                { title: { [Op.like]: searchString } },
+                { featured: featured },
+                { yield: { [Op.like]: searchString } },
+                { cook_time: searchVal },
+                { cuisine: { [Op.like]: searchString } },
+                { description: { [Op.like]: searchString } },
+                { direction_list: { [Op.like]: searchString } },
+                { ingredient_list: { [Op.like]: searchString } }
             ]
-        })
-            .then(dbGetData => {
-                const recipe = dbGetData.map(recipes => recipes.get({ plain: true }));
+        },
+        include: [
+            {
+                model: User,
+                attributes: ["id", "username"]
+            },
+            { model: Ingredients },
+            { model: Directions }
+        ]
+    })
+        .then(dbGetData => {
+            const recipe = dbGetData.map(recipes => recipes.get({ plain: true }));
 
-                res.render('search-results', {
-                    recipe,
-                    loggedIn: req.session.loggedIn
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
+            res.render('search-results', {
+                recipe,
+                loggedIn: req.session.loggedIn
             });
-    });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
 
 
-    router.get('/recipepage/:id', (req, res) => {
-        Recipes.findOne({
-            where: {
-                id: req.params.id
+router.get('/recipepage/:id', (req, res) => {
+    Recipes.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No recipe found with this id' });
+                return;
             }
-        })
-            .then(dbPostData => {
-                if (!dbPostData) {
-                    res.status(404).json({ message: 'No recipe found with this id' });
-                    return;
-                }
 
-                const recipe = dbPostData.get({ plain: true });
+            const recipe = dbPostData.get({ plain: true });
 
-                res.render('recipe-page', {
-                    recipe,
-                    loggedIn: req.session.loggedIn,
-                    user_email: req.session.user_email,
-                    isAdmin: req.session.isAdmin
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
+            res.render('recipe-page', {
+                recipe,
+                loggedIn: req.session.loggedIn,
+                user_email: req.session.user_email,
+                isAdmin: req.session.isAdmin
             });
-    })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
 
-    router.get('/edit-recipe/:id', (req, res) => {
-        Recipes.findOne({
-            where: {
-                id: req.params.id
+router.get('/edit-recipe/:id', (req, res) => {
+    Recipes.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Ingredients
             },
-            include: [
-                {
-                    model: Ingredients
-                },
-                {
-                    model: Directions
-                }
-            ]
-        })
-            .then(dbPostData => {
-                if (!dbPostData) {
-                    res.status(404).json({ message: 'No recipe found with this id' });
-                    return;
-                }
-
-                const recipe = dbPostData.get({ plain: true });
-
-                res.render('editRecipe', {
-                    recipe,
-                    loggedIn: req.session.loggedIn,
-                    isAdmin: req.session.isAdmin
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json(err);
-            });
+            {
+                model: Directions
+            }
+        ]
     })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No recipe found with this id' });
+                return;
+            }
 
-    module.exports = router;
+            const recipe = dbPostData.get({ plain: true });
+
+            res.render('editRecipe', {
+                recipe,
+                loggedIn: req.session.loggedIn,
+                isAdmin: req.session.isAdmin
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
+
+module.exports = router;
