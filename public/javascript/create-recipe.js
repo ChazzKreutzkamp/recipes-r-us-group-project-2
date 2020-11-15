dbbf86e0b451e6bf2a521774f5e94115054662d4
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-11-15 15:54:55 -0700

    add image upload functionality

--- public/javascript/create-recipe.js
+++ public/javascript/create-recipe.js
@@ -13,10 +13,12 @@ async function postRecipeHandler(event) {
     const image_filename = document.querySelector('#input_img').value;
 
 
-    //for demo
+    //simpler recipe list placeholders
     const direction_list = document.querySelector('#input_direction_list').value;
     const ingredient_list = document.querySelector('#input_ingredients_list').value;
 
+    var file = document.getElementById("input_img").files[0];
+    console.log("name : " + file.name);
 
     const response = await fetch(`/api/recipes`, {
         method: 'POST',
@@ -26,7 +28,7 @@ async function postRecipeHandler(event) {
             cook_time,
             description,
             cuisine,
-            image_filename,
+            image_filename: file.name,
             direction_list,
             ingredient_list
             // user_id handled by api
@@ -44,5 +46,57 @@ async function postRecipeHandler(event) {
         alert(response.statusText);
     }
 }
+
+async function uploadImage(event) {
+
+    var file = this.files[0];
+    console.log("name : " + file.name);
+
+
+    var form = document.getElementById("testForm");
+    var formData = new FormData(form);
+    // console.log("submitted?")
+    const response = await fetch('/api/recipes/uploadImage', {
+        method: 'POST',
+        body: formData
+    });
+    if (response.ok) {
+        console.log("success");
+        // document.location.replace('/homepage');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+// async function postRecipeImageHandler() {
+//     var file = this.files[0];
+//     // This code is only for development
+//     console.log("name : " + file.name);
+//     console.log("size : " + file.size);
+//     console.log("type : " + file.type);
+//     console.log("date : " + file.lastModified);
+
+
+
+
+//     const response = await fetch(`/api/recipes`, {
+//         method: 'POST',
+//         body: formData,
+//         headers: {
+//             'Content-Type': 'multipart/form-data'
+//         }
+//     });
+//     // could there be a directions page?
+//     //With the way the api and models are set up this is one solution
+//     if (response.ok) {
+//         console.log("success");
+//         document.location.replace('/homepage');
+//     } else {
+//         alert(response.statusText);
+//     }
+// }
+
+document.getElementById('input_img').addEventListener('change', uploadImage);
+
 
 document.querySelector('#new_recipe_submit_button').addEventListener('click', postRecipeHandler);
